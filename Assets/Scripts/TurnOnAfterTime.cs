bb32c585bd4eddf58c7ed14032bc065224d20d70
Harrison McNeil <harrisonmcneildesign@gmail.com>
2021-05-12 08:25:22 +1000

    =Times dialogue

---
+++ Assets/Scripts/TurnOnAfterTime.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurnOnAfterTime : MonoBehaviour {
+
+	
+
+    public GameObject toggleObject;
+    public float textBegintime;
+	public float textLifetime;
+
+    void OnTriggerEnter (Collider other)
+	{
+		if (other.tag == "Player")
+		{
+			
+			StartCoroutine(SelfToggleOn());
+			StartCoroutine(SelfToggleOff());
+			
+		}
+		
+	}
+
+
+	IEnumerator SelfToggleOn()
+    {
+		yield return new WaitForSeconds(textBegintime);
+		toggleObject.SetActive(true);
+    }
+
+	IEnumerator SelfToggleOff()
+    {
+		yield return new WaitForSeconds(textLifetime);
+		toggleObject.SetActive(false);
+    }
+}
