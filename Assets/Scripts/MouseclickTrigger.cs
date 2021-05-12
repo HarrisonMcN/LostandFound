7bbc43ec81da84b53f0ab994ff7c100539802b62
Harrison McNeil <harrisonmcneildesign@gmail.com>
2021-05-12 10:27:22 +1000

    Help me dear god

---
+++ Assets/Scripts/MouseclickTrigger.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseclickTrigger : MonoBehaviour
+{
+    public GameObject text;
+    public float textLifetime;
+
+
+    void OnMouseDown()
+    {
+        text.SetActive(true);
+        StartCoroutine(SelftToggleOff());
+    }
+
+    IEnumerator SelftToggleOff()
+    {
+        yeild return new WaitForSeconds(textLifetime);
+        toggleObject.SetActive(false);
+    }   
+}
